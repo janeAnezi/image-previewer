a4a5c252df22910f6551f82977e0ed611790a323
Anezijane <ahnnieanezi@gmail.com>
2024-12-03 11:34:41 +0100

    add state to manage images

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
+import { useState } from "react";
 import { FaRegPlayCircle } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
+import {images} from "./constants/index"
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState('assets/food1.jpg')
+
   return (
     <main className="h-screen bg-slate-300 relative pt-28">
       <button className=" absolute right-64 top-10 text-yellow-600 hover:text-teal-500"><FaRegPlayCircle /></button>
       
       <div className="flex justify-center items-center ">
-        <div className="w-80 ">
-          <img src="assets/food1.jpg" alt="food" className="w-full rounded-md" />
-        </div>
-      </div>
-      <div className="flex justify-between">
-        <button className="text-yellow-600 absolute left-48 bottom-48 hover:text-teal-500" >
+        <button className="text-yellow-600 hover:text-teal-500 pr-4" >
           <FaArrowLeft />
         </button>
-
-        <button className="text-yellow-600 absolute right-48 bottom-48 hover:text-teal-500">
+        <div className="w-80 ">
+          <img src={selectedImage} alt="food" className="w-full rounded-md" />
+        </div>
+        <button className="text-yellow-600  hover:text-teal-500 pl-4">
           <FaArrowRight />
         </button>
+      </div>
+
+      <div className="mt-6">
+        {images.map((image, ))}
       </div>
         
     </main>
---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const images = [
+    '../../public/assets/food1.jpg',
+    '../../public/assets/food2.jpg',
+    '../../public/assets/food3.jpg',
+    '../../public/assets/food4.jpg',
+    '../../public/assets/food5.jpg',
+    '../../public/assets/food6.jpg',
+    '../../public/assets/food7.jpg',
+    '../../public/assets/food8.jpg',
+    '../../public/assets/food9.jpg',
+    '../../public/assets/food10.jpg',
+]

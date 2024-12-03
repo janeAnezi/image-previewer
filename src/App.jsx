d1acf69d16318b4dd0229f7f47d253a8f2bffcd0
Anezijane <ahnnieanezi@gmail.com>
2024-12-03 11:03:22 +0100

    add icon styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { FaArrowLeft } from "react-icons/fa";
 function App() {
   return (
     <main className="h-screen bg-slate-300 relative pt-28">
-      <button className=" absolute right-64 top-10 text-yellow-600"><FaRegPlayCircle /></button>
+      <button className=" absolute right-64 top-10 text-yellow-600 hover:text-teal-500"><FaRegPlayCircle /></button>
       
       <div className="flex justify-center items-center ">
         <div className="w-80 ">
@@ -13,11 +13,11 @@ function App() {
         </div>
       </div>
       <div className="flex justify-between">
-        <button className="text-yellow-600 absolute left-48 bottom-48" >
+        <button className="text-yellow-600 absolute left-48 bottom-48 hover:text-teal-500" >
           <FaArrowLeft />
         </button>
 
-        <button className="text-yellow-600 absolute right-48 bottom-48">
+        <button className="text-yellow-600 absolute right-48 bottom-48 hover:text-teal-500">
           <FaArrowRight />
         </button>
       </div>

732b123638a31c8faf02c95203fba5cd66b905e8
Anezijane <ahnnieanezi@gmail.com>
2024-12-03 15:33:10 +0100

    create constants and map throught the images

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { FaArrowLeft } from "react-icons/fa";
 import {images} from "./constants/index"
 
 function App() {
-  const [selectedImage, setSelectedImage] = useState('assets/food1.jpg')
+  const [selectedImage, setSelectedImage] = useState(images[0])
 
   return (
-    <main className="h-screen bg-slate-300 relative pt-28">
+    <main className="h-screen relative pt-28">
       <button className=" absolute right-64 top-10 text-yellow-600 hover:text-teal-500"><FaRegPlayCircle /></button>
       
       <div className="flex justify-center items-center ">
@@ -23,8 +23,16 @@ function App() {
         </button>
       </div>
 
-      <div className="mt-6">
-        {images.map((image, ))}
+      <div className="mt-6 ml-8">
+        {images.map((image,index) => (
+          <button>
+            <img 
+              src={image} 
+              alt={`image ${index + 1}`}
+              className="w-32 h-20 object-cover hover:scale-125 transition duration-700 ease-in-out rounded-md"
+            />
+          </button>
+        ))}
       </div>
         
     </main>

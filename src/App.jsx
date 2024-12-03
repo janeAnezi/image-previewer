95b4a23023ca34f02e4ce5c525fc218abb5b042b
Anezijane <ahnnieanezi@gmail.com>
2024-12-03 10:54:25 +0100

    add images and main content

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { FaRegPlayCircle } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
+
+function App() {
+  return (
+    <main className="h-screen bg-slate-300 relative pt-28">
+      <button className=" absolute right-64 top-10 text-yellow-600"><FaRegPlayCircle /></button>
+      
+      <div className="flex justify-center items-center ">
+        <div className="w-80 ">
+          <img src="assets/food1.jpg" alt="food" className="w-full rounded-md" />
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <button className="text-yellow-600 absolute left-48 bottom-48" >
+          <FaArrowLeft />
+        </button>
+
+        <button className="text-yellow-600 absolute right-48 bottom-48">
+          <FaArrowRight />
+        </button>
+      </div>
+        
+    </main>
+  )
+}
+
+export default App
